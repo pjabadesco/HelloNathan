d71857a648cf214d179898af64c9e8f008c612cc
Paolo Josef Abadesco <pjabadesco@Paolo-Josef-Abadescos-MacBook-Pro.local>
2011-09-21 02:05:20 +0800

    Initial Commit

---
+++ HelloNathan/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  HelloNathan
+//
+//  Created by Paolo Josef Abadesco on 9/21/11.
+//  Copyright Neuralink IT Solutions 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit HelloNathan.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ HelloNathan/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloNathan
+//
+//  Created by Paolo Josef Abadesco on 9/21/11.
+//  Copyright Neuralink IT Solutions 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
